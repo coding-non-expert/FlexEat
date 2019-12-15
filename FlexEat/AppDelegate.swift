f8317030e63ccae6f8620340435b4f764149507d
celeste <celiresmart@gmail.com>
2019-12-16 03:58:24 +0800

    removed firebase references

--- FlexEat/AppDelegate.swift
+++ FlexEat/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //Firebase Library:
-        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
