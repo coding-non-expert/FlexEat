bdc6bff7f418adb0c492787b5ec342eb83abbe28
celeste <celiresmart@gmail.com>
2019-12-16 18:55:34 +0800

    set up video playing for starting view controller but the video is not chosen yet

--- FlexEat/View Controllers/StartingViewController.swift
+++ FlexEat/View Controllers/StartingViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import AVKit
 
 class StartingViewController: UIViewController {
+    
+    var videoPlayer: AVPlayer?
+    
+    var videoPlayerLayer: AVPlayerLayer?
 
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //Uncomment the line below and the function below to enable video playing
+        //setUpVideo()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +29,45 @@ class StartingViewController: UIViewController {
     }
     
     func setUpElements() {
+        
         Utilities.styleHollowButton(signUpButton)
         Utilities.styleFilledButton(loginButton)
+        
     }
+    
+    /*func setUpVideo() {
+        
+        //Get path to the resource in the bundle
+        let bundlePath = Bundle.main.path(forResource: <#T##String?#>, ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        
+        //Create url
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        //Create player item
+        let item = AVPlayerItem(url: url)
+        
+        //Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        //Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        //Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5,
+                                         y: 0,
+                                         width: self.view.frame.size.width*4,
+                                         height: self.frame.view.size.height)
+     
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        //Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+        
+    }*/
 
     /*
     // MARK: - Navigation
